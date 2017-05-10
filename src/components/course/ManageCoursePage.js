2d029c8c7772322993196a8034dca28df0738f84
Piotr Grzesik <pj.grzesik@gmail.com>
2017-05-10 21:00:56 +0200

    Add populating form if clicking on existing course

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -53,8 +53,19 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
+
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+  if (courseId) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
