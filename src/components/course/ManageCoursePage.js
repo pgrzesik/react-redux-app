0a02ee62bc3f87eef2e392d7c760fbf71e831387
Piotr Grzesik <pj.grzesik@gmail.com>
2017-05-11 17:00:02 +0200

    Add test for checking if course name is longer than 5 characters as well as form validator that checks that

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -5,7 +5,7 @@ import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
@@ -18,6 +18,7 @@ class ManageCoursePage extends React.Component {
     this.updateCourseState = this.updateCourseState.bind(this);
     this.saveCourse = this.saveCourse.bind(this);
     this.redirect = this.redirect.bind(this);
+    this.courseFormIsValid = this.courseFormIsValid.bind(this);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -33,8 +34,26 @@ class ManageCoursePage extends React.Component {
     return this.setState({course: course});
   }
 
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
+
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+
+    this.setState({errors: errors});
+    return formIsValid;
+  }
+
   saveCourse(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
+
     this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
       .then(() => this.redirect())
