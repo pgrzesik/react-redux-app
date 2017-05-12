58990a2bd9fe9a1fdd4cd7cf7343184d042b21af
Piotr Grzesik <pj.grzesik@gmail.com>
2017-05-12 18:36:05 +0200

    Add store test

---
+++ src/store/store.test.js
@@ -0,0 +1,24 @@
+import expect from 'expect';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+describe('Store', () => {
+  it('should handle creating courses', () => {
+    const store = createStore(rootReducer, initialState);
+    const course = {
+      title: 'Clean code'
+    };
+
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action);
+
+    const actual = store.getState().courses[0];
+    const expected = {
+      title: 'Clean code'
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
